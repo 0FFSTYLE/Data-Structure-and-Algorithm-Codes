7b13da4ba5487d27de91ecf62090a5da66f163fc
= <=>
2026-07-02 23:00:49 +0530

    2jul2026

--- temp.cpp
+++ temp.cpp
@@ -1,53 +1,19 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int add(int num1, int num2);
-int sub(int num1, int num2);
-int mul(int num1, int num2);
-int div(int num1, int num2);
-int rem(int num1, int num2);
+int fib(int value);
 
 int main(){
-    int num1, num2;
-
-    cout<<"Enter first number : "; cin>> num1;
-    cout<<"Enter second number : "; cin>> num2;
-
-    cout<<add(num1,num2)<<endl;
-    cout<<sub(num1,num2)<<endl;
-    cout<<mul(num1,num2)<<endl;
-    cout<<div(num1,num2)<<endl;
-    cout<<rem(num1,num2)<<endl;
+    fib(8);
+    return 0;
 }
 
-int add(int num1, int num2){
-    return num1 + num2;
-}
-
-int sub(int num1, int num2){
-    return num1 - num2;
-}
-
-int mul(int num1, int num2){
-    return num1*num2;
-}
-
-int div(int num1, int num2){
-    if (num2 == 0)
+int fib(int value){
+    for (int i = 0; i < value; i++)
     {
-        cout<<"Cannot divide by 0 ";
-    }
-    else{
-        return num1/num2;
+        cout<<i;
+        i += i;
     }
     
-}
-
-int rem(int num1, int num2){
-    if(num2 == 0){
-         cout<<"Cannot divide by 0, unable to process remainder ";
-    }
-    else{
-        return num1%num2;
-    }
 }
